8fe1c5a4c3dfd17c6b96d1812a485165d21d870f
unknown <balbis.rb@gmail.com>
2019-02-13 11:22:41 +0100

    first

---
+++ chat-master/Personne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Personne
+    {
+        public string nickname { get; set; }
+        public int id { get; set; }
+        public List<string> messageList { get; set; }
+
+        public Personne(int id, string nick)
+        {
+            this.id = id;
+            this.nickname = nick;
+            this.messageList = new List<string>();
+        }
+
+        public Personne(int id, string nick, List<string> msglist)
+        {
+            this.id = id;
+            this.nickname = nick;
+            this.messageList = msglist;
+
+        }
+    }
+}
---
+++ chat-master/VMPersonneMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class VMPersonneMessage
+    {
+
+        public ObservableCollection<Personne> contactList { get; set; }
+
+        public VMPersonneMessage()
+        {
+            contactList = new ObservableCollection<Personne>();
+            Personne p1 = new Personne(1, "mich", new List<string> { "hello", "hi", "ho" });
+            Personne p2 = new Personne(2, "didier", new List<string> { "Salut c didier", "hai" });
+            contactList.Add(p1);
+            contactList.Add(p2);
+        }
+
+        
+
+    }
+}
